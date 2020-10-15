60f4d7f28ec23552b088766e4d65cdf452fa3718
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-10-15 16:13:16 +0700

    tags template literals

--- 08 template_literals/script.js
+++ 08 template_literals/script.js
@@ -10,16 +10,63 @@ const n = 10
 console.log(`${ ( n > 12 ) ? 'Kurang dari 12': 'Lebih dari 12' }`)
 
 // HTML Fragment
-const people = {
-    name: 'People',
-    age: 22,
-    nrp: '0012000',
-    email: 'people@people.com'
-}
+const list_people = [
+    {
+        name: 'People',
+        email: 'people@people.com'
+    },
+    {
+        name: 'Doddi',
+        email: 'doddy@doddy.com'
+    },
+    {
+        name: 'Erik',
+        email: 'erik@erik.com'
+    }
+
+]
 
 const el = `
     <div class="people">
-        <h2>${people.nama}</h2>
-        <span>${people.nrp}</span>
+    ${list_people.map(people => `
+        <h2>${people.name}</h2>
+        <span>${people.email}</span>    
+    `).join("")}
     </div>
 `
+
+document.querySelector('.el1').innerHTML = el
+
+// Nested Html Fragment
+
+const author = {
+    name: 'Some Author',
+    desc: 'Description of some author',
+    books: [
+        'Book One',
+        'Fantasy Universe',
+        'End World',
+        'Signature'
+    ]
+}
+
+function displayListOfBooks(books) {
+    return `
+        <ol>
+        ${books.map(book => `
+            <li>${book}</li>
+        `).join('')}
+        </ol>
+    `
+}
+
+const el2 = `
+    <div class="author">
+        <h2>${author.name}</h2>
+        <p>${author.desc}</p>
+        <h4>List of Book: </h4>
+        ${displayListOfBooks(author.books)}
+    </div>
+`
+
+document.querySelector('.el2').innerHTML = el2
